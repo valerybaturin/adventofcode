19d166fcc082c15705a403da65caa63034ccd1ae
Valery Baturin <valery@baturin.dev>
2021-04-18 19:15:15 +0300

    Solve part 1

--- 2015/day_1/part_1/main.go
+++ 2015/day_1/part_1/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Welcome to AoC 2015")
+	// Reading file to variable.
+	f, err := os.ReadFile("../input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Calculating floor and printing answer in terminal.
+	fmt.Println(CalculateFloor(string(f)))
+}
+
+func CalculateFloor(s string) int {
+	// He starts on the ground floor (floor 0)
+	// and then follows the instructions one character at a time.
+	var floor int
+
+	for _, v := range s {
+		// An opening parenthesis, (, means he should go up one floor,
+		// and a closing parenthesis, ), means he should go down one floor.
+		if string(v) == "(" {
+			floor++
+		}
+		if string(v) == ")" {
+			floor--
+		}
+	}
+
+	// Returning calculated floor
+	return floor
 }
---
+++ 2015/day_1/part_1/main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestCalculateFloor(t *testing.T) {
+	// (()) and ()() both result in floor 0.
+	// ((( and (()(()( both result in floor 3.
+	// ))((((( also results in floor 3.
+	// ()) and ))( both result in floor -1 (the first basement level).
+	// ))) and )())()) both result in floor -3.
+	cases := []struct {
+		symbol   string
+		expected int
+	}{
+		{symbol: "(())", expected: 0},
+		{symbol: "()()", expected: 0},
+		{symbol: "(()(()(", expected: 3},
+		{symbol: "(((", expected: 3},
+		{symbol: "))(((((", expected: 3},
+		{symbol: "())", expected: -1},
+		{symbol: "))(", expected: -1},
+		{symbol: ")))", expected: -3},
+		{symbol: ")())())", expected: -3},
+	}
+
+	for _, c := range cases {
+		t.Run(fmt.Sprintf("string_%s", c.symbol), func(t *testing.T) {
+			actual := CalculateFloor(c.symbol)
+			if actual != c.expected {
+				t.Errorf("expected %v, got %v", c.expected, actual)
+			}
+		})
+	}
+}
